5ef1bcc4bfb4af5d37124d2f9dc6d6d78e22c953
Cesar <cesario46@outlook.com>
2024-11-13 18:10:42 -0400

    agregue al app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { libroModule } from './Libros/libro.module';
 
 @Module({
-  imports: [],
+  imports: [libroModule],
   controllers: [AppController],
   providers: [AppService],
 })
